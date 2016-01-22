7f9bb33720a49d52001dc0150d0eac5d227ef0fc
ccarruitero <cesar@mozilla.pe>
2016-01-21 23:40:57 -0500

    just needs an api for now

--- app.rb
+++ app.rb
@@ -19,33 +19,7 @@ Dir['./lib/**/*.rb'].each { |f| require f }
 Dir['./models/**/*.rb'].each { |f| require f }
 Dir['./routes/**/*.rb'].each { |f| require f }
 
-Cuba.use Rack::Static,
-  urls: %w(/css /js /img),
-  root: './public'
-
 Cuba.define do
-  on get do
-    on root do
-      email = session[:email]
-      res.write view('index', email: email)
-    end
-
-    if !login?
-      res.redirect '/'
-    end
-  end
-
-  on post do
-    on 'login' do
-      run Login
-    end
-
-    on 'logout' do
-      session[:email] = nil
-      res.redirect '/'
-    end
-  end
-
   on 'products' do
     run Products
   end
